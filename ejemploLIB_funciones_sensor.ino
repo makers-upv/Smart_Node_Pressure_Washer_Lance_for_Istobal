32a1558b14e16eafb941c2752594630209a2cb1a
luismartinez0711 <102617405+luismartinez0711@users.noreply.github.com>
2022-05-31 16:56:48 +0200

    Add files via upload

---
+++ ejemploLIB_funciones_sensor.ino
@@ -0,0 +1,15 @@
+//LA LECTRUA APARECE EN EL MONITOR SERIE 
+
+#include "libreria_gestos.h"
+  
+void setup() {
+
+inicializar_sensor(); //llama a la función que inicializa el sensor
+
+}
+
+void loop() {
+
+flag_sensor();
+
+}
