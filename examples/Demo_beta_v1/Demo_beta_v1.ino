6486c6153c4f515b4a04ab9c849029c0b6a9746d
XxAndres0211xX <arb02112001@gmail.com>
2022-06-14 10:50:21 +0200

    Demo actualizado
    Maquina de estados completa excepto RF y con botones emulando

--- examples/Demo_beta_v1/Demo_beta_v1.ino
+++ examples/Demo_beta_v1/Demo_beta_v1.ino
@@ -1,11 +1,17 @@
+
 #define USE_TIMER_2 true
 #include <megaAVR_TimerInterrupt.h>
 #include <megaAVR_TimerInterrupt.hpp>
 #define TIMER2 1           //ATENCION: Frecuencia, no segundos, no milsiegundos
 #include "tftLibrary.hpp"
+//Lirberias gestos
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+#define APDS9960_INT    3
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
 
 enum estados {SOPORTE, STANDBY, LAVADO};
-enum eventos {CARGANDO, NO_CARGANDO, SALDO, START, NEXT, BACK, END, TIEMPO};
+enum eventos {CARGANDO, NO_CARGANDO, SALDO, START, UP, DOWN, END, TIEMPO, GESTO, GESTOERROR};
 
 struct maquina_estados_struct {
   estados estado;         //Estado actual en el que nos encontramos
@@ -23,7 +29,9 @@ const byte interruptPin_Cargando = A3;
 const byte interruptPin_Moneda = A2;
 
 void Proceso_Maquina(maquina_estados_struct *Maquina_Estados_puntero);
-
+//Variables generales
+int ModoTrabajo = 1, CounterTime = 0;
+#define TIEMPO_LAVADO 60
 void interruptHandler_Gestos() {
   interruptOccurred_Gestos = true;
 }
@@ -34,20 +42,19 @@ void interruptHandler_Cargando() {
   Maquina_Estados.flag_evento = true;
 }
 
-void interruptHandler_Gesture_but() {
-  Maquina_Estados.evento = START;
+void interruptHandler_Gesture() {
+  Maquina_Estados.evento = GESTO;
   Maquina_Estados.flag_evento = true;
 }
 
 void interruptHandler_Timer2() {
-  Maquina_Estados.evento = TIEMPO;
+  Maquina_Estados.evento = TIEMPO;    
   Maquina_Estados.flag_evento = true;
 }
 
 void interruptHandler_Moneda() {
   Maquina_Estados.evento = START;
   Maquina_Estados.flag_evento = true;
-  Serial.println("Hola");
 }
 
 Screen screen;
@@ -59,23 +66,49 @@ void setup() {
   pinMode(interruptPin_Cargando, INPUT_PULLUP);
   attachInterrupt(interruptPin_Cargando, interruptHandler_Cargando, CHANGE);
   attachInterrupt(interruptPin_Moneda, interruptHandler_Moneda, FALLING);
+
   ITimer2.init();
   ITimer2.pauseTimer();
-  if (ITimer2.attachInterrupt(TIMER2, interruptHandler_Timer2)) Serial.print("Inciado el temporizador de puta madre");  //ATENCION: Frecuencia, no segundos, no milsiegundos
+  if (ITimer2.attachInterrupt(TIMER2, interruptHandler_Timer2)) Serial.println("Inciado el temporizador de puta madre");  //ATENCION: Frecuencia, no segundos, no milsiegundos
   else Serial.println("Ha habido un error con el timer");
   screen.begin();
+  //Gestos
+  pinMode(APDS9960_INT, INPUT);
+  attachInterrupt(APDS9960_INT, interruptHandler_Gesture, FALLING);
+  if ( apds.init() ) Serial.println(F("APDS-9960 initialization complete"));
+  else     Serial.println(F("Something went wrong during APDS-9960 init!"));
+  if ( apds.enableGestureSensor(true) )    Serial.println(F("Gesture sensor is now running"));
+  else    Serial.println(F("Something went wrong during gesture sensor init!"));
+
 }
 
 void loop() {
+  //Maquina de estados
   if (Maquina_Estados.flag_evento == true) {
     Maquina_Estados.flag_evento = false;
+    if (Maquina_Estados.evento == GESTO) LeerGesto();
     Proceso_Maquina(&Maquina_Estados);
   }
 }
 
+void LeerGesto() {
+  detachInterrupt(APDS9960_INT);
+  if ( apds.isGestureAvailable() ) {
+    switch ( apds.readGesture() ) {
+      case DIR_UP:
+        Maquina_Estados.evento = UP;
+        break;
+      case DIR_DOWN:
+        Maquina_Estados.evento = DOWN;
+        break;
+      default:
+        Maquina_Estados.evento = GESTOERROR;
+    }
+    attachInterrupt(APDS9960_INT, interruptHandler_Gesture, FALLING);
+  }
+}
 
-
-void Proceso_Maquina(maquina_estados_struct* Maquina_Estados_puntero) {
+void Proceso_Maquina(maquina_estados_struct * Maquina_Estados_puntero) {
   switch (Maquina_Estados_puntero->estado) {
     case SOPORTE:
       switch (Maquina_Estados_puntero->evento) {
@@ -88,37 +121,36 @@ void Proceso_Maquina(maquina_estados_struct* Maquina_Estados_puntero) {
           delay(1000);
           Maquina_Estados.estado = STANDBY;
           break;
-
         default:
           break;
-
       }
       break;
 
     case STANDBY:
-      switch (Maquina_Estados_puntero->evento)
-      {
+      switch (Maquina_Estados_puntero->evento)        {
         case CARGANDO:
           screen.begin();
-          break;
 
-        case TIEMPO:
-          screen.updateScreen();
+          Maquina_Estados.estado = SOPORTE;
           break;
 
         case START:
+          //Preparamos la pantalla
           screen.setTemplate(4);
           screen.setModeHeader(20, WHITE, BLACK);
           screen.setMode(62, 80, WHITE, BLACK);
           screen.setPrice(10, 119, TEMPLATERED, WHITE);
           screen.setTimer(75, 119, WHITE, TEMPLATERED);
-          ITimer2.resumeTimer();
+          ITimer2.resumeTimer();  //Iniciamos el temporizador
+          ModoTrabajo = 1;
+          CounterTime = 0;
+          screen.changeModeTo(ModoTrabajo);
+          Maquina_Estados.estado = LAVADO;
           break;
 
         default:
 
           break;
-
       }
 
       break;
@@ -126,28 +158,34 @@ void Proceso_Maquina(maquina_estados_struct* Maquina_Estados_puntero) {
     case LAVADO:
       switch (Maquina_Estados_puntero->evento)
       {
-
-        case BACK:
-
+        case TIEMPO:
+          //Actualiza el tiempo y el dinero
+          screen.updateScreen();
+          CounterTime++;
+          if (CounterTime >= TIEMPO_LAVADO){
+            screen.nextStartBMP();
+            ITimer2.pauseTimer();
+            Maquina_Estados.estado = STANDBY;
+          }
           break;
 
-        case NEXT:
+        case UP:
+          if(ModoTrabajo <= 4)  ModoTrabajo++;
+          screen.changeModeTo(ModoTrabajo);
+          break;
 
+        case DOWN:
+          if(ModoTrabajo >= 2)  ModoTrabajo--;
+          screen.changeModeTo(ModoTrabajo);
           break;
 
         default:
 
           break;
-
       }
-
-
       break;
-
     default:
-
       break;
-
   }
 
 
